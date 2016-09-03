6341a37bc73bbbf9db4605ccdc282ca9663845cd
ganj28 <anushag@Sivas-MacBook-Pro.local>
2016-09-03 16:18:47 -0700

    some packages

--- app_stock.py
+++ app_stock.py
@@ -1,7 +1,7 @@
 import simplejson as json
 from flask import Flask, render_template, request
 import requests
-import dateutil
+from dateutil.relativedelta import *
 import datetime
 import pandas as pd
 import numpy as np
