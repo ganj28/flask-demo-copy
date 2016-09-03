71024a6c544b4f7351b3ab0cf5c22901b458cbd5
ganj28 <anushag@Sivas-MacBook-Pro.local>
2016-09-03 16:46:14 -0700

    some packages

--- app_stock.py
+++ app_stock.py
@@ -5,8 +5,9 @@ from dateutil.relativedelta import *
 import datetime
 import pandas as pd
 import numpy as np
-from bokeh.plotting import figure, output_file, show
+#from bokeh.plotting import figure, output_file, show
 from bokeh.embed import components
+import bokeh.plotting
 
 app_stock = Flask(__name__)
 
