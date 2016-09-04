4421a680fd0d8965803fa3d8773eb46f5f76248a
ganj28 <anushag@Sivas-MacBook-Pro.local>
2016-09-03 18:19:25 -0700

    trial

--- app_stock.py
+++ app_stock.py
@@ -6,7 +6,7 @@ import datetime
 import pandas as pd
 import numpy as np
 #from bokeh.plotting import figure, output_file, show
-from bokeh.embed import components
+#from bokeh.embed import components
 
 app_stock = Flask(__name__)
 
