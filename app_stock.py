220a743bd91fdceb699e40c766f155ba44cc799f
ganj28 <anushag@Sivas-MacBook-Pro.local>
2016-09-03 20:44:36 -0700

    please work

--- app_stock.py
+++ app_stock.py
@@ -73,7 +73,7 @@ def stock_trend():
 		p.line(x, adjopen, legend="Adj. Opening Price", line_width=2, color='olivedrab')
 
 	script, div = components(p)
-	return render_template('trend.html', script=script, div=div)
+	return render_template('stock_trend.html', script=script, div=div)
 
 if __name__ == "__main__":
 	app_stock.run(port=33507)
